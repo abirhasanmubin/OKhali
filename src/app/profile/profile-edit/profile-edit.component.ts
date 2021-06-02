8cbe791d006d46e846fa64b9322402e6fc3a7cd1
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-06-02 17:28:43 +0600

    profile - edit complete

--- src/app/profile/profile-edit/profile-edit.component.ts
+++ src/app/profile/profile-edit/profile-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile-edit',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileEditComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  profileForm: FormGroup;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('userData'));
+    this.profileForm = new FormGroup({
+      'name': new FormControl(this.user.userFullName),
+      'contact': new FormControl(this.user.userContactNo),
+    })
+  }
+
+  onSubmit() {
+    let name = this.profileForm.value['name'];
+    let contact = this.profileForm.value['contact'];
+    this.user.userFullName = name;
+    this.user.userContactNo = contact;
+    localStorage.setItem('userData', JSON.stringify(this.user));
+    this.userService.updateUser(this.user.userId, this.user);
+    this.router.navigate(['/profile']);
+    this.profileForm.reset();
   }
 
 }
