7b9430ec2c8abc90eb28a44d1831f50e702e2db8
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-06-02 12:56:36 +0600

    trip - edit data prepopulate done

--- src/app/trips/trip-edit/trip-edit.component.ts
+++ src/app/trips/trip-edit/trip-edit.component.ts
@@ -22,44 +22,49 @@ export class TripEditComponent implements OnInit, OnDestroy {
     private tripService: TripService,
     private router: Router,
     private route: ActivatedRoute
-  ) { }
-
-  ngOnInit(): void {
+  ) {
     this.route.params.subscribe((params: Params) => {
       this.id = params['id'];
       this.editMode = params['id'] != null;
       if (this.editMode) {
-        this.tripService.getTrip(this.id).subscribe(data => {
+        this.tripSub = this.tripService.getTrip(this.id).subscribe(data => {
           this.trip = data;
+          this.initForm();
         })
       }
-      this.initForm();
+      else {
+        this.initForm();
+      }
     })
+  }
 
+  ngOnInit(): void {
   }
 
   initForm() {
     let from = '';
     let to = '';
     let date = null;
-    // if (this.editMode) {
-    //   this.tripForm = new FormGroup({
-    //     'from': new FormControl(this.trip.tripFrom, Validators.required),
-    //     'to': new FormControl(this.trip.tripTo, Validators.required),
-    //     'date': new FormControl(this.trip.tripDate, Validators.required),
-    //   });
-    // }
-    // else {
-    this.tripForm = new FormGroup({
-      'from': new FormControl('', Validators.required),
-      'to': new FormControl('', Validators.required),
-      'date': new FormControl('', Validators.required),
-    });
-    // }
+    if (this.editMode) {
+      let time = new Date(this.trip.tripDate.toDate()).toISOString().slice(0, 10);
+      this.tripForm = new FormGroup({
+        'from': new FormControl(this.trip.tripFrom, Validators.required),
+        'to': new FormControl(this.trip.tripTo, Validators.required),
+        'date': new FormControl(time, Validators.required),
+      });
+    }
+    else {
+      this.tripForm = new FormGroup({
+        'from': new FormControl('', Validators.required),
+        'to': new FormControl('', Validators.required),
+        'date': new FormControl('', Validators.required),
+      });
+    }
   }
 
   onSubmit() {
     if (this.tripForm.valid) {
+
       let from = this.tripForm.value['from'];
       let to = this.tripForm.value['to'];
       let date = new Date(this.tripForm.value['date']);
