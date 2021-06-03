b0e4b21e212bcb69177f874c3c39429a94e2210c
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-06-03 16:25:34 +0600

    ride - service done

--- src/app/models/ride.model.ts
+++ src/app/models/ride.model.ts
@@ -1,6 +1,8 @@
 export class Ride {
   constructor(
     public rideId: string,
+    public rideFrom: string,
+    public rideTo: string,
     public rider: string,
     public startTime: any,
     public endTime: any,
--- src/app/services/ride.service.ts
+++ src/app/services/ride.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Ride } from '../models/ride.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RideService {
 
-  constructor() { }
+  rides: Observable<Ride[]>;
+  ride: Observable<Ride>;
+  rideCollection: AngularFirestoreCollection<Ride>;
+
+  constructor(
+    private firestore: AngularFirestore,
+  ) {
+    this.rideCollection = this.firestore.collection<Ride>('rides');
+
+    this.rides = this.rideCollection.snapshotChanges()
+      .pipe(map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Ride;
+          return data;
+        })
+      }));
+  }
+
+  getRides() {
+    return this.rides;
+  }
+
+  getRide(id: string) {
+    this.ride = this.rideCollection.doc(id).snapshotChanges()
+      .pipe(map(action => {
+        const data = action.payload.data() as Ride;
+        return data;
+      }))
+
+    return this.ride;
+  }
+  addRide(ride: Ride) {
+    const id = this.firestore.createId();
+    ride.rideId = id;
+    return this.rideCollection.doc(id).set(Object.assign({}, ride)).then(() => {
+      return id;
+    });
+  }
+
+  updateRide(id: string, ride: Ride) {
+    return this.rideCollection.doc(id).set(Object.assign({}, ride));
+  }
+
+  deleteRide(id: string) {
+    return this.rideCollection.doc(id).delete();
+  }
+
 }
